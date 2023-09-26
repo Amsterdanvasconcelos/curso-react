dd190b9c9081a5f5bbd2dfca3ac799a4c043cd36
Amsterdan Vasconcelos <135554841+Amsterdanvasconcelos@users.noreply.github.com>
2023-09-26 07:03:02 -0300

    feat: create Pokemon component and creat pages and components folders

--- src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>Meu site React</h1>
-  );
-}
-
-export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Pokemon } from '../components/Pokemon'
+
+export const Home = () => {
+  return (
+    <header>
+      <h1>Meu site React</h1>
+      <Pokemon/>
+    </header>
+  ) 
+}
